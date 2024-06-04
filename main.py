e712953242ad6a9713d2de24bd8e6025492a35a1
LeiChen9 <chenlei9691@gmail.com>
2024-06-04 21:05:07 +0800

    add keywords extraction prompt

--- main.py
+++ main.py
@@ -10,10 +10,15 @@
  10 
  11 def model_mentor(model, history):
  12     # prompt = "告诉我今天上海的天气如何."
+        prompt = "Please respond only in the Chinese language. Do not explain what you are doing. \
+                  Do not self reference. You are an expert text analyst. \
+                  Please summary the theme of the dialog and extract only the most relevant keywords \
+                  and key phrases from a piece of text. Please showcase the results in 3 list: \
+                  theme, keywords, key phrases. Please analyze the following text: "
  13     messages = [
  14         {"role": "system", "content": "You are a mentor of executive model. Your job is extracting, organizing, analyzing and summarizing the history information, and distill important information for executive model\
  15                                         and make him works better."},
- 16         {"role": "user", "content": history}
+ 16         {"role": "user", "content": prompt + history}
  17     ]
  18     text = tokenizer.apply_chat_template(
  19         messages,
@@ -33,11 +38,13 @@
  33     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
  34     return response
  35 
- 36 def model_chat(model, prompt):
+ 36 def model_chat(model, prompt, input_text):
  37     # prompt = "告诉我今天上海的天气如何."
  38     messages = [
- 39         {"role": "system", "content": "You are a helpful assistant."},
- 40         {"role": "user", "content": prompt}
+ 39         {"role": "system", "content": "You are a helpful assistant. Based on the input text and key messages in your head about previous dialog, \
+                                you need to provide a response."},
+            {"role": "system", "content": "history theme, key words and key phrases are {}".format(prompt)}
+ 40         {"role": "user", "content": input_text}
  41     ]
  42     text = tokenizer.apply_chat_template(
  43         messages,
