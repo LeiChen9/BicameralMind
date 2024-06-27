8a59c8684b192a50314c8eeace40012c78f81146
LeiChen9 <chenlei9691@gmail.com>
2024-06-28 07:55:56 +0800

    finish agent manager implementation

--- agents/agent_manager.py
+++ agents/agent_manager.py
@@ -2,7 +2,7 @@
 Author: LeiChen9 chenlei9691@gmail.com
 Date: 2024-06-27 21:57:01
 LastEditors: LeiChen9 chenlei9691@gmail.com
-LastEditTime: 2024-06-27 22:06:25
+LastEditTime: 2024-06-28 07:55:19
 FilePath: /Code/BicameralMind/agents/agent_manager.py
 Description: 
 
@@ -22,23 +22,29 @@ class AgentManager():
         """Initialize the Agent manager."""
         self._agent_obj_map: dict[str, Agent] = {}
     
-    def register(self, component_instance_name: str, component_instance_obj: ComponentTypeVar):
-        """Register the component instance."""
-        pass
+    def register(self, agent_name: str, agent_obj: Agent):
+        """Register the agent instance."""
+        if agent_name in self._agent_obj_map:
+            return
+        self._agent_obj_map[agent_name] = agent_obj
 
-    def unregister(self, component_instance_name: str):
-        """Unregister the component instance abstractmethod."""
-        pass
+    def unregister(self, agent_name: str):
+        """Unregister the agent instance."""
+        if agent_name not in self._agent_obj_map:
+            return
+        del self._agent_obj_map[agent_name]
 
-    def get_instance_obj(self, component_instance_name: str,
-                         appname: str = None, new_instance: bool = None) -> ComponentTypeVar:
-        """Return the component instance object."""
-        pass
+    def get_instance_obj(self, agent_name: str,
+                         appname: str = None, new_instance: bool = None) -> Agent:
+        """Return the agent instance object."""
+        if agent_name not in self._agent_obj_map:
+            raise Exception(f"Agent {agent_name} not found.")
+        return self._agent_obj_map[agent_name]
 
-    def get_instance_name_list(self) -> list[str]:
-        """Return the component instance list."""
-        pass
+    def get_agent_name_list(self) -> list[str]:
+        """Return the agent instance list."""
+        return list(self._agent_obj_map.keys())
 
-    def get_instance_obj_list(self) -> list[ComponentTypeVar]:
-        """Return the component instance object list."""
-        pass
+    def get_agent_obj_list(self) -> list[Agent]:
+        """Return the agent instance object list."""
+        return list(self._agent_obj_map.values())
