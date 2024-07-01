75bd5fdd3c1b396a258fa262b564668164e91ae8
LeiChen9 <chenlei9691@gmail.com>
2024-07-01 13:35:52 +0800

    init eval

---
+++ eval.py
@@ -0,0 +1,10 @@
+'''
+Author: LeiChen9 chenlei9691@gmail.com
+Date: 2024-07-01 13:33:16
+LastEditors: LeiChen9 chenlei9691@gmail.com
+LastEditTime: 2024-07-01 13:35:40
+FilePath: /SpeechDepDiag/Users/lei/Documents/Code/BicameralMind/eval.py
+Description: 
+
+Copyright (c) 2024 by Riceball, All Rights Reserved. 
+'''
