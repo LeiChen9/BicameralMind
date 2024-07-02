3f7265cc822185adb8420a1c78efd05c98847df5
LeiChen9 <chenlei9691@gmail.com>
2024-07-02 11:34:19 +0800

    trying to load pubmedqa as eval

--- eval.py
+++ eval.py
@@ -2,9 +2,17 @@
 Author: LeiChen9 chenlei9691@gmail.com
 Date: 2024-07-01 13:33:16
 LastEditors: LeiChen9 chenlei9691@gmail.com
-LastEditTime: 2024-07-01 13:35:40
+LastEditTime: 2024-07-02 11:24:22
 FilePath: /SpeechDepDiag/Users/lei/Documents/Code/BicameralMind/eval.py
 Description: 
 
 Copyright (c) 2024 by Riceball, All Rights Reserved. 
 '''
+
+import json 
+import pdb
+
+with open("datasets/pubmedqa/data/pqal_fold0/dev_set.json", 'r') as f:
+    data = json.load(f)
+    
+pdb.set_trace()
