30cdb2b3da1a5118b7dfe0f0a8c8ba03c44f9e88
LeiChen9 <chenlei9691@gmail.com>
2024-07-04 16:57:26 +0800

    init main

---
+++ LangChain/main.py
@@ -0,0 +1,11 @@
+'''
+Author: LeiChen9 chenlei9691@gmail.com
+Date: 2024-07-04 14:51:16
+LastEditors: LeiChen9 chenlei9691@gmail.com
+LastEditTime: 2024-07-04 16:42:45
+FilePath: /SpeechDepDiag/Users/lei/Documents/Code/BicameralMind/LangChain/main.py
+Description: 
+
+Copyright (c) 2024 by Riceball, All Rights Reserved. 
+'''
+
